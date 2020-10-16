f0d97076a3d01cb8bb375ac6e353ed0af4345f2f
CharonShen <61600619@qq.com>
2020-10-16 10:02:38 +0800

    add

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
 
 function App() {
-  this.handleClick = this.handleClick.bind(this);
-  handleClick() {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  }
   return (
     
-    activateLasers
     <div className="App">
       <button onClick={activateLasers}> 
         连接钱包
