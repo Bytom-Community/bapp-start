2f84a236410651b0a4915f2e9905dcee307f841b
CharonShen <61600619@qq.com>
2020-10-16 09:45:22 +0800

    add

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 function App() {
+
   return (
+    
     <div className="App">
-      <button id="byone-add">连接Byone钱包</button>
+      <button id="byone-add"> </button>
     </div>
   );
 }
 
 export default App;
+document.addEventListener('chromeBytomLoaded', bytomExtension => {
+  window.bytom.enable().then(accounts => {
+    window.accounts = accounts
+  })
+});
