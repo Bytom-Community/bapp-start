6884c2f3bea90842a1c761efbd07cd36b9290004
CharonShen <61600619@qq.com>
2020-10-28 16:47:48 +0800

    update

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,14 +5,28 @@ function App() {
 
   const [toAddress,setAddress] = useState('');
   const [amount,setAmount] = useState('');
+  const [password,setPassword] = useState('');
+  const [total,setTotalword] = useState('');
+  const [program,setProgramword] = useState('');
 
   function handleAddressInput(event) {
     setAddress(event.target.value);
   }
 
-
   function handleAmountInput(event) {
     setAmount(event.target.value);
+  }
+
+  function handlePasswordInput(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleTotalInput(event) {
+    setTotalword(event.target.value);
+  }
+
+  function handleProgramInput(event) {
+    setProgramword(event.target.value);
   }
 
   //连接钱包
@@ -47,20 +61,63 @@ function App() {
 
     var params = {
        "to":toAddress,
-       "from": window.bytom.default_account.accountId,
+       "from": window.bytom.default_account.address,
        "amount":amount,
        "asset":"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
     }
 
-    console.log(window.bytom.default_account.accountId);
+    //console.log(window.bytom.default_account.accountId);
 
     return window.bytom.sendTransaction(params).then(function (resp) {
         alert(resp[0].transactionHash)
       }).catch(function (err){
-        alert(err)
+       alert(err)
       })
     
     };
+
+    const sendContract = async () => {
+      var params = {
+        input:[
+         {"amount":total,"asset":"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff","type":"spend_wallet"}
+         ],
+       output:[
+         {"amount":total,"asset":"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff","controlProgram":program,"type":"control_program"}
+         ],
+        gas: '0.00400000'
+       }
+
+      return window.bytom.sendAdvancedTransaction(params).then(function (resp) {
+        alert(resp.transactionHash)
+      }).catch(function (err){
+        alert(err)
+      })
+    },
+
+    const unlockContract = async () => {
+      var params = {
+        input:[
+         {"amount":total,"asset":"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff","type":"spend_wallet"}
+         ],
+       output:[
+         {"amount":total,"asset":"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff","controlProgram":program,"type":"control_program"}
+         ]  ,
+        gas: '0.00400000',
+        args:[
+            { 
+            "type":"String",
+            "rawData":{ 
+               "value":sha3_256(password),
+              }
+            }
+          ]
+       }
+      return window.bytom.sendAdvancedTransaction(params).then(function (resp) {
+        alert(resp.transactionHash)
+      }).catch(function (err){
+        alert(err)
+      })
+    },
 
   return (
     
@@ -72,6 +129,16 @@ function App() {
       <p>输入发送数量</p>
       <input onChange={handleAmountInput}></input>
       <button onClick={sendTX}>发送交易</button>
+      
+      <p>输入存入数量</p>
+      <input onChange={handleTotalInput}></input>
+      <p>输入合约程序</p>
+      <input onChange={handleProgramInput}></input>
+
+      <button onClick={sendContract}>存入合约</button>
+      <p>输入口令</p>
+      <input onChange={handlePasswordInput}></input>
+      <button onClick={unlockContract}>解锁合约资产</button>
     </div>
   );
 }
